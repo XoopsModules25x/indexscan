c781c30416f0af6c59804d78700c6e02bc486157
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-09-07 13:51:14 +0000

    Frensh Translation Thanks to kris_fr

---
+++ XoopsModules/indexscan/language/french/admin.php
@@ -0,0 +1,23 @@
+<?php
+//%%%%%%		English language file for index scan / admin 		%%%%%
+define("_AM_INDEXSCAN_NOW","Scanner pour rep&#233;rer les fichiers index absents");
+define("_AM_INDEXSCAN_CREATE","Cr&#233;er un fichier index o&#249; ils sont absents");
+define("_AM_INDEXSCAN_HELP","Aide");
+define("_AM_INDEXSCAN_CONFIG","Configuration");
+define("_AM_INDEXSCAN_HEADER","Ces dossiers ne poss&#232;dent pas de fichiers index");
+define("_AM_INDEXSCAN_NOTFOUND","Fichiers index <strong>non</strong> trouv&#233;s<br />\n");
+define("_AM_INDEXSCAN_FOUNDMISSING","<strong>Absence</strong> de  fichiers index trouv&#233;e\n");
+define("_AM_INDEXSCAN_MAKINGHEADER","Cr&#233;ation des fichiers index.html absents");
+define("_AM_INDEXSCAN_CREATED","index.html cr&#233;&#233;");
+define("_AM_INDEXSCAN_CREATEDINDEXFILES","Fichier index cr&#233;&#233;");
+
+/**
+ * @translation     AFUX (Association Francophone des Utilisateurs de Xoops) (http://www.afux.org/)
+ * @specification   _LANGCODE: fr
+ * @specification   _CHARSET: UTF-8
+ *
+ * @Translator      Kris (kris@afux.org)
+ *
+ * @version         $Id
+**/
+?>
---
+++ XoopsModules/indexscan/language/french/modinfo.php
@@ -0,0 +1,29 @@
+<?php
+
+// English language file for indexscan
+
+define("_MI_INDEXSCAN_MAIN","Accueil");
+define("_MI_INDEXSCAN_SCANNOW","Scanner maintenant");
+define("_MI_INDEXSCAN_HELP","Aide");
+define("_MI_INDEXSCAN_SETTINGS","Pr&#233;f&#233;rences");
+define("_MI_INDEXSCAN_MODULE_NAME","Indexscan");
+define("_MI_INDEXSCAN_MODULE_DESC","Scanne votre Xoops pour rep&#233;rer les fichiers<br /> index absents. Si n&#39;existe pas, le module peut les cr&#233;er.");
+define("_MI_INDEXSCAN_EXEP1","Dossier &#224; ne pas scanner 01");
+define("_MI_INDEXSCAN_EXEP1_DESC","S&#39;il y a des dossiers que vous ne d&#233;sirez pas scanner (Par exemple : <strong>uploads</strong>)<br /> vous devez inscrire son nom ici");
+define("_MI_INDEXSCAN_EXEP2","Dossier &#224; ne pas scanner 02");
+define("_MI_INDEXSCAN_EXEP2_DESC","");
+define("_MI_INDEXSCAN_EXEP3","Dossier &#224; ne pas scanner 03");
+define("_MI_INDEXSCAN_EXEP3_DESC","");
+define("_MI_INDEXSCAN_EXEP4","Dossier &#224; ne pas scanner 04");
+define("_MI_INDEXSCAN_EXEP4_DESC","");
+
+/**
+ * @translation     AFUX (Association Francophone des Utilisateurs de Xoops) (http://www.afux.org/)
+ * @specification   _LANGCODE: fr
+ * @specification   _CHARSET: UTF-8
+ *
+ * @Translator      Kris (kris@afux.org)
+ *
+ * @version         $Id
+**/
+?>
