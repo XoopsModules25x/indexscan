433c8584c06f8df39283cbef45189c95fb230792
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-09-12 08:03:07 +0000

    Dutch translation for indexscan // Thanks Reintjan

---
+++ XoopsModules/indexscan/language/nederlands/admin.php
@@ -0,0 +1,13 @@
+<?php
+//%%%%%%		Nederlands taalbestand voor index scan / administratie 		%%%%%
+define("_AM_INDEXSCAN_NOW","Scan op ontbrekende index bestanden");
+define("_AM_INDEXSCAN_CREATE","Maak index bestanden aan waar deze ontbreken");
+define("_AM_INDEXSCAN_HELP","Help");
+define("_AM_INDEXSCAN_CONFIG","Configuratie");
+define("_AM_INDEXSCAN_HEADER","In deze mappen ontbreken index bestanden");
+define("_AM_INDEXSCAN_NOTFOUND","Index bestanden <b>niet</b> gevonden<br>\n");
+define("_AM_INDEXSCAN_FOUNDMISSING","<b>ontbrekende</b> index bestanden gevonden\n");
+define("_AM_INDEXSCAN_MAKINGHEADER","Bezig ontbrekende index.html bestanden aan te maken");
+define("_AM_INDEXSCAN_CREATED","index.html aangemaakt");
+define("_AM_INDEXSCAN_CREATEDINDEXFILES","index bestand aangemaakt");
+?>
---
+++ XoopsModules/indexscan/language/nederlands/modinfo.php
@@ -0,0 +1,20 @@
+<?php
+
+// Nederlands taalbestand voor indexscan
+
+define("_MI_INDEXSCAN_MAIN","Start");
+define("_MI_INDEXSCAN_SCANNOW","Scan nu");
+define("_MI_INDEXSCAN_CREATEINDEX", "Maak index bestanden");
+define("_MI_INDEXSCAN_HELP","Help");
+define("_MI_INDEXSCAN_SETTINGS","Instellingen");
+define("_MI_INDEXSCAN_MODULE_NAME","Indexscan");
+define("_MI_INDEXSCAN_MODULE_DESC","Scant uw Xoops installatie op ontbrekende<br> index bestanden. Wanneer enkele ontbreken kunt u deze laten aanmaken.");
+define("_MI_INDEXSCAN_EXEP1","Niet te scannen map 01");
+define("_MI_INDEXSCAN_EXEP1_DESC","Wanneer er mappen zijn die u niet wilt laten scannen, bijvoorbeeld <b>uploads</b><br> dan vult u de naam van die map hier in");
+define("_MI_INDEXSCAN_EXEP2","Niet te scannen map 02");
+define("_MI_INDEXSCAN_EXEP2_DESC","");
+define("_MI_INDEXSCAN_EXEP3","Niet te scannen map 03");
+define("_MI_INDEXSCAN_EXEP3_DESC","");
+define("_MI_INDEXSCAN_EXEP4","Niet te scannen map 04");
+define("_MI_INDEXSCAN_EXEP4_DESC","");
+?>
