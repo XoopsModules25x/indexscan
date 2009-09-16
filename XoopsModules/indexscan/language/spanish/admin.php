8da634b522434a3f384995d5daa633b42ccdb16a
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-09-16 20:52:09 +0000

    Spanish translation // thanks to DonCurioso at Hispaxoops

---
+++ XoopsModules/indexscan/language/spanish/admin.php
@@ -0,0 +1,20 @@
+<?php
+//%%%%%%		Lenguaje español para index scan / admin desde HispaXoops - www.hispaxoops.com		%%%%%
+define("_AM_INDEXSCAN_NOW","Escanear archivos index perdidos");
+define("_AM_INDEXSCAN_CREATE","Crea archivos index.html donde hagan falta");
+define("_AM_INDEXSCAN_HELP","Ayuda");
+define("_AM_INDEXSCAN_CONFIG","Configurar");
+define("_AM_INDEXSCAN_HEADER","Estas carpetas han perdido o no tienen sus archivos index.html");
+define("_AM_INDEXSCAN_NOTFOUND","<b>No</b> Encontrado<br>\n");
+define("_AM_INDEXSCAN_FOUNDMISSING","<b>Archivos index.html</b> encontrados\n");
+define("_AM_INDEXSCAN_MAKINGHEADER","Creando archivos index.html");
+define("_AM_INDEXSCAN_CREATED","index.html creados");
+define("_AM_INDEXSCAN_CREATEDINDEXFILES","Creado el generador de archivos index.html");
+
+// Added in 1.01
+define("_AM_INDEXSCAN_CHECKFORFILES","<tr class='header'><center><h2>Chequear archivos con posible inyección de IFRAME</h2></tr></center><center><small>No todos usan inyección de IFRAME.<br>Chequea el código en los archivos antes de que los borres.<br><hr></small></center>");
+define("_AM_INDEXSCAN_UNABLETOREADFILE","No se puede abrir el archivo ");
+define("_AM_INDEXSCAN_INFECTED","!!Palabra en IFRAME encontrada!!");
+define("_AM_INDEXSCAN_CLEAN","OK");
+define("_AM_INDEXSCAN_INJECTIONSCAN","Escanear inyecciones de IFRAME");
+?>
